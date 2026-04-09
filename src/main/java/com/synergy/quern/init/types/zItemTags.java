537fc88a5b16879fb065c13fb158a89d7cfbfdb1
DevDyna <andrearisorti2003@gmail.com>
2026-04-10 00:18:08 +0200

    itemtags , dusts flour and wooden gear

---
+++ src/main/java/com/synergy/quern/init/types/zItemTags.java
@@ -0,0 +1,12 @@
+package com.synergy.quern.init.types;
+
+import com.synergy.quern.init.Material;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class zItemTags {
+    public static final TagKey<Item> DUST_IRON = Material.tagItem("dusts/iron", "c");
+    public static final TagKey<Item> DUST_GOLD = Material.tagItem("dusts/gold", "c");
+    public static final TagKey<Item> DUST_COPPER = Material.tagItem("dusts/copper", "c");
+}
--- src/main/java/com/synergy/quern/init/types/zItems.java
+++ src/main/java/com/synergy/quern/init/types/zItems.java
@@ -2,17 +2,29 @@ package com.synergy.quern.init.types;
 
 import static com.synergy.quern.Main.ID;
 
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class zItems {
 
     public static void register(IEventBus bus) {
         zItem.register(bus);
+        zIngredients.register(bus);
         zBlockItem.register(bus);
     }
 
+    public static final DeferredRegister.Items zIngredients = DeferredRegister.createItems(ID);
     public static final DeferredRegister.Items zItem = DeferredRegister.createItems(ID);
     public static final DeferredRegister.Items zBlockItem = DeferredRegister.createItems(ID);
+
+    public static final DeferredItem<Item> WOODEN_GEAR = zIngredients.registerSimpleItem("wooden_gear");
+  
+    public static final DeferredItem<Item> DUST_IRON = zIngredients.registerSimpleItem("iron_dust");
+    public static final DeferredItem<Item> DUST_COPPER = zIngredients.registerSimpleItem("copper_dust");
+    public static final DeferredItem<Item> DUST_GOLD = zIngredients.registerSimpleItem("gold_dust");
+  
+    public static final DeferredItem<Item> FLOUR = zIngredients.registerSimpleItem("flour");
 
 }
