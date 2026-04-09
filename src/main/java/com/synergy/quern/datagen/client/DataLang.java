1c88b7f2a65ea2bb50d8d0fe2cd5b61d5a959f21
DevDyna <andrearisorti2003@gmail.com>
2026-04-10 00:18:47 +0200

    Update DataLang.java

--- src/main/java/com/synergy/quern/datagen/client/DataLang.java
+++ src/main/java/com/synergy/quern/datagen/client/DataLang.java
@@ -1,12 +1,13 @@
 package com.synergy.quern.datagen.client;
 
-
 import static com.synergy.quern.Main.ID;
 
 import com.synergy.quern.init.types.zBlocks;
+import com.synergy.quern.init.types.zItems;
 
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.LanguageProvider;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class DataLang extends LanguageProvider {
 
@@ -20,11 +21,27 @@ public class DataLang extends LanguageProvider {
         protected void addTranslations() {
 
                 addBlock(zBlocks.QUERN, "Quern");
+
+                zItems.zIngredients.getEntries().forEach(i -> addItem(i, named(i)));
+
+                advKey("quern", "It spin!", "Craft a quern to process resources into dusts");
+
         }
 
-        
+        private String named(DeferredHolder<?, ?> b) {
 
-        @SuppressWarnings("unused")
+                StringBuilder result = new StringBuilder();
+                for (String word : b.getRegisteredName()
+                                .replace(ID + ":", "")
+                                .replaceAll("_", " ")
+                                .split(" "))
+                        if (!word.isEmpty())
+                                result.append(Character.toUpperCase(word.charAt(0)))
+                                                .append(word.substring(1))
+                                                .append(" ");
+                return result.toString().trim();
+        }
+
         private void advKey(String k, String title, String desc) {
                 add(ID + ".advancement.branch." + k, title);
                 add(ID + ".advancement.branch." + k + ".desc", desc);
